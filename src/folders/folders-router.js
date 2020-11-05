956fd3d03f126831dc1e446aa32cb1fd28315402
Alanna Bouloy <lanihope@gmail.com>
2020-11-05 15:30:06 -0500

    added GET /:folder_id endpoint and test

--- src/folders/folders-router.js
+++ src/folders/folders-router.js
@@ -17,4 +17,29 @@ foldersRouter
             .catch(next)
     })
 
+foldersRouter
+    .route('/:folder_id')
+    .all((req, res, next) => {
+        const knexInstance = req.app.get('db')
+        FoldersService.getById(knexInstance, req.params.folder_id)
+            .then(folder => {
+                if(!folder){
+                    return res
+                        .status(404)
+                        .json({error: { message: 'Folder Not Found' } })
+                }
+                res.folder = folder
+                next()
+
+            })
+            .catch(next)
+    })
+    .get((req, res, next) => {
+        res.json({
+            id: res.folder.id,
+            folder_name: res.folder.folder_name
+        })
+        .catch(next)
+    })
+
 module.exports = foldersRouter
--- test/folders-endpoint.spec.js
+++ test/folders-endpoint.spec.js
@@ -22,7 +22,7 @@ describe(`/api/folders endpoints work`, () => {
 
     afterEach('cleanup', () => db.raw('TRUNCATE noteful_folders, noteful_notes RESTART IDENTITY CASCADE'))
 
-    describe(`GET api/folders endpoint works`, () => {
+    describe(`GET /api/folders endpoint works`, () => {
 
         context(`when there are folders in the database`, () => {
             const testFolders = makeFoldersArray()
@@ -46,6 +46,64 @@ describe(`/api/folders endpoints work`, () => {
                     .get('/api/folders')
                     .expect(200, [])
             })
+        })
+    })
+
+    describe.only(`GET /api/folders/:folder_id`, () => {
+        context(`there are folders in the database`, () => {
+            const testFolders = makeFoldersArray()
+
+            beforeEach('insert articles', () => {
+                return db
+                    .into('noteful_folders')
+                    .insert(testFolders)
+            })
+
+            it(`responds with 200 and the specified folder`, () => {
+                const folderId = 2
+                const expectedFolder = testFolders[folderId - 1]
+
+                return supertest(app)
+                    .get(`/api/folders/${folderId}`)
+                    .expect(200, expectedFolder)
+            })
+        })
+
+        context(`the database is empty`, () => {
+            it(`responds with a 404 message`, () => {
+                const folderId = 12345
+                const errorMessage = { error : { message: `Folder Not Found`} }
+
+                return supertest(app)
+                    .get(`/api/folders/${folderId}`)
+                    .expect(404, errorMessage)
+            })
+
+        })
+    })
+   
+    describe(`POST /api/folders endpoint works`, () => {
+        it(`returns a 201 and correct information when request valid`, () => {
+            const newFolder = {
+                folder_name: 'Test Folder added'
+            }
+            return supertest(app)
+                .post('/api/folders')
+                .send(newFolder)
+                .expect(201)
+                .expect(res => {
+                    expect(res.body.folder_name).to.eql(newFolder.folder_name)
+                    expect(res.body).to.have.property('id')
+                })
+                .then(postRes => {
+                    return supertest(app)
+                        .get(`/articles/${postRes.body.id}`)
+                        .expect(postRes.body)
+                })
+
+        })
+        it(`returns a 400 bad request when given invalid data`, () => {
+
         })
     })
 })
